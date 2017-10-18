7c5cdad463a74cc4e6eeadbd01c37993dcaa2f2e
daliannyvieira <dalianny-sb@hotmail.com>
2017-10-18 16:19:31 -0300

    first

---
+++ src/components/Hello.vue
@@ -0,0 +1,35 @@
+<template>
+  <section>
+    <ul>
+      <li v-for="resposta in respostas">
+        <p><strong>{{resposta.name}}</strong></p>
+        <p>{{resposta.email}}</p>
+      </li>
+    </ul>
+  </section>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'hello',
+  data () {
+    return {
+      respostas: [],
+    }
+  },
+  created() {
+    axios.get(`http://jsonplaceholder.typicode.com/users`)
+    .then(response => {
+      this.respostas = response.data
+    })
+  }
+}
+</script>
+
+<style>
+ul {  
+  list-style: none;
+}
+</style>
